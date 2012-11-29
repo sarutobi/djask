6442f4ccbf37cd593a61c2f46100eb65dfa3e6b1
Valery A. Ilychev <ilychev@gmail.com>
2012-11-29 11:56:51 +0400

    Initial commit

---
+++ djask/urls.py
@@ -0,0 +1,17 @@
+from django.conf.urls import patterns, include, url
+
+# Uncomment the next two lines to enable the admin:
+# from django.contrib import admin
+# admin.autodiscover()
+
+urlpatterns = patterns('',
+    # Examples:
+    # url(r'^$', 'djask.views.home', name='home'),
+    # url(r'^djask/', include('djask.foo.urls')),
+
+    # Uncomment the admin/doc line below to enable admin documentation:
+    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+
+    # Uncomment the next line to enable the admin:
+    # url(r'^admin/', include(admin.site.urls)),
+)
