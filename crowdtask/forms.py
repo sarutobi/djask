d0a3da4e72f763a4c340a2c55f2587ecd6e8483a
Valery A. Ilychev <ilychev@gmail.com>
2013-01-16 14:23:53 +0400

    Add form description

--- crowdtask/forms.py
+++ crowdtask/forms.py
@@ -6,12 +6,15 @@ from crowdtask.models import Application, Task
 
 
 class ApplicationForm(forms.ModelForm):
+    '''Create application form'''
     class Meta:
         model = Application
+        widgets = {"presenter": forms.HiddenInput(),}
         exclude = ['user', 'really_finished']
 
 
 class TaskForm(forms.ModelForm):
+    '''Create task form'''
     class Meta:
         model = Task
         exclude = ['user', 'creation_time', 'status', 'current_runs']
